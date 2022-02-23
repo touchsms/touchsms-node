193378a5f86878b522ebc1686db7988888776020
duncanm <github@duncanmargetts.com>
2022-02-24 09:29:21 +1030

    convert SDK to https (#6)
    Co-authored-by: duncanrm <duncan.margetts@sa.gov.au>
    Co-authored-by: atymic <atymicq@gmail.com>

--- index.js
+++ index.js
@@ -1,108 +1,107 @@
-'use strict';
+"use strict";
 
-var http = require('http');
-var querystring = require('querystring');
-var q = require('q');
+var https = require("https");
+var querystring = require("querystring");
+var q = require("q");
 
-var touchSMS = function(access_token, token_id, sandbox) {
+var touchSMS = function (access_token, token_id, sandbox) {
+  if (sandbox === true) {
+    var url = "sandbox.touchsms.com.au";
+  } else {
+    var url = "platform.touchsms.com.au";
+  }
 
-    if (sandbox === true) {
-        var url = 'sandbox.touchsms.com.au';  
-    } else {
-        var url = 'platform.touchsms.com.au';    
-    }    
+  this.sendSms = function (options, callback) {
+    var postBuild = [];
 
-    this.sendSms = function(options, callback) {
-        var postBuild = [];
-
-        if (options.number != undefined) {
-            postBuild['number'] = options.number;
-        }
-
-        if (options.message != undefined) {
-            postBuild['message'] = options.message;
-        }
-
-        if (options.senderid != undefined) {
-            postBuild['senderid'] = options.senderid;
-        }
-
-        if (options.reference != undefined) {
-            postBuild['reference'] = options.reference;
-        }
-
-        if (options.campaignName != undefined) {
-            postBuild['campaignName'] = options.campaignName;
-        }
-
-        var postData = querystring.stringify(postBuild);
-
-        var options = {
-            auth: access_token + ':' + token_id,
-            host: url,
-            path: '/rest/v1/messages',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': postData.length
-            }
-        };
-
-        var deferred = q.defer();
-
-        var req = http.request(options, (res) => {
-            var body = '';
-
-            res.setEncoding('utf8');
-            res.on('data', (data) => {
-                body += data;
-            });
-            res.on('end', () => {
-                deferred.resolve(body);
-            });
-        });
-
-        req.on('error', (err) => {
-            deferred.reject(err);
-        });
-
-        req.write(postData);
-        req.end();
-
-        return deferred.promise;
+    if (options.number != undefined) {
+      postBuild["number"] = options.number;
     }
 
-    this.users = function(callback) {
-        var options = {
-            auth: access_token + ':' + token_id,
-            host: url,
-            path: '/rest/v1/users',
-            method: 'GET'
-        };
+    if (options.message != undefined) {
+      postBuild["message"] = options.message;
+    }
 
-        var deferred = q.defer();
+    if (options.senderid != undefined) {
+      postBuild["senderid"] = options.senderid;
+    }
 
-        var req = http.request(options, (res) => {
-            var body = '';
+    if (options.reference != undefined) {
+      postBuild["reference"] = options.reference;
+    }
 
-            res.setEncoding('utf8');
-            res.on('data', (data) => {
-                body += data;
-            });
-            res.on('end', () => {
-                deferred.resolve(body);
-            });
-        });
+    if (options.campaignName != undefined) {
+      postBuild["campaignName"] = options.campaignName;
+    }
 
-        req.on('error', (err) => {
-            console.log(err);
-            deferred.reject(err);
-        });
+    var postData = querystring.stringify(postBuild);
 
-        req.end();
+    var options = {
+      auth: access_token + ":" + token_id,
+      host: url,
+      path: "/rest/v1/messages",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": postData.length,
+      },
+    };
 
-        return deferred.promise;
-    } 
-}
+    var deferred = q.defer();
+
+    var req = https.request(options, (res) => {
+      var body = "";
+
+      res.setEncoding("utf8");
+      res.on("data", (data) => {
+        body += data;
+      });
+      res.on("end", () => {
+        deferred.resolve(body);
+      });
+    });
+
+    req.on("error", (err) => {
+      deferred.reject(err);
+    });
+
+    req.write(postData);
+    req.end();
+
+    return deferred.promise;
+  };
+
+  this.users = function (callback) {
+    var options = {
+      auth: access_token + ":" + token_id,
+      host: url,
+      path: "/rest/v1/users",
+      method: "GET",
+    };
+
+    var deferred = q.defer();
+
+    var req = https.request(options, (res) => {
+      var body = "";
+
+      res.setEncoding("utf8");
+      res.on("data", (data) => {
+        body += data;
+      });
+      res.on("end", () => {
+        deferred.resolve(body);
+      });
+    });
+
+    req.on("error", (err) => {
+      console.log(err);
+      deferred.reject(err);
+    });
+
+    req.end();
+
+    return deferred.promise;
+  };
+};
 
 module.exports = touchSMS;
